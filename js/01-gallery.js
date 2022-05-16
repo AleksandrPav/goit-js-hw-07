1533e1d787a335498c3510408b2435a904c0ad0d
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-05-16 14:03:14 +0300

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,9 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const refs = {
+    iamgeOut: document.querySelector(`.gallery`),
+}
+
+
+
+console.log(galleryItems);
