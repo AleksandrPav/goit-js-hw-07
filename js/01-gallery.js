98866ed749d4ee99a0f25fc35567858aa5052594
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-05-17 14:28:58 +0300

    17/05/22

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const refs = {
-    iamgeOut: document.querySelector(`.gallery`),
+    paletteContainer: document.querySelector(`.gallery`),
+    cardsMarkup: createImagesMarkup(galleryItems),
+    link: document.querySelector(`.gallery__link`)
 }
+console.log(refs.link)
+// refs.link.addEventListener(`click`, removeLink) 
+// const removeLink = function (e) {
+//         e.preventDefault();
+//     };   
 
 
+refs.paletteContainer.insertAdjacentHTML(`beforeend`, refs.cardsMarkup);
 
-console.log(galleryItems);
+function createImagesMarkup(images) {
+    
+    return images
+        .map(({ preview, original, description }) =>
+`<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+         />
+  </a>
+</div>`
+    ).join(``);
+    
+    
+}
